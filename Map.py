165c9de8245f157b7de1e909a85d912017d760ca
chipoltie0 <chipoltie0@gmail.com>
2016-08-03 22:49:06 -0400

    add print statement

--- Map.py
+++ Map.py
@@ -213,7 +213,7 @@ class OrganicGenerator:
         # get location to start growing room
         start_point = random.uniform(0,len(seed_room.edges))
         #Test comment horay!
-        print("horay!")
+        print("horay!!")
 
 class Partition:
 
